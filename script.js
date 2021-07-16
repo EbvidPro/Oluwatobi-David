d266a72ce8e9ee526d5ade7fc9c713663b3c17d8
EbvidPro <davidebenezer93@gmail.com>
2021-07-16 17:29:22 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Navbar scrolling effect
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById('navbar').style.top = '0';
+        document.getElementById('navbar').style.backgroundColor = 'black'
+    } else {
+        document.getElementById('navbar').style.top = '-100px';
+    }
+    prevScrollpos = currentScrollPos;
+}
+// Mobile Navbar 
+function menu() {
+    let hamburger = document.querySelector('.hamburg');
+    let closeBtn = hamburger.getAttribute('src');
+    let mobileMenu = document.querySelector('.menu');
+
+    if (closeBtn == "icon-hamburger.svg") {
+        hamburger.setAttribute('src', 'icon-close.svg');
+        mobileMenu.style.display = 'block';
+    } else {
+        hamburger.setAttribute('src', 'icon-hamburger.svg');
+        mobileMenu.style.display = 'none';
+    }
+}
