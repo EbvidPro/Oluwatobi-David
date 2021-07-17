466b5c034e77533d6a5856e6655b035247db7bb1
EbvidPro <davidebenezer93@gmail.com>
2021-07-17 13:06:55 +0100

    Update

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById('navbar').style.top = '0';
-        document.getElementById('navbar').style.backgroundColor = 'black'
     } else {
         document.getElementById('navbar').style.top = '-100px';
     }
